9d925c8ac16e21d49bdf89583f2d72e780beb8db
vu09040072304 <vu09040072304@gmail.com>
2023-02-15 17:50:22 +0700

    fb login loi

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,20 @@ import {
 } from '@react-native-google-signin/google-signin';
 
 import {ViewStyle} from 'react-native/Libraries/StyleSheet/StyleSheetTypes';
+import {
+  Settings,
+  LoginButton,
+  AccessToken,
+  LoginManager,
+} from 'react-native-fbsdk-next';
 
 export const App = () => {
   useEffect(() => {
     GoogleSignin.configure({
       webClientId: '',
     });
+    Settings.setAppID('APP ID');
+    Settings.initializeSDK();
   }, []);
   const signIn = async () => {
     try {
@@ -38,10 +46,65 @@ export const App = () => {
       }
     }
   };
+  const signIn_Fb = () => {
+    LoginManager.logInWithPermissions([
+      'public_profile',
+      'email',
+      'user_friends',
+    ]).then(
+      function (result) {
+        if (result.isCancelled) {
+          console.log('Login cancelled');
+        } else {
+          AccessToken.getCurrentAccessToken()
+            .then((data: any) => {
+              console.log('Val ', data);
+
+              console.log(data.accessToken.toString());
+            })
+            .catch(e => {
+              console.log('E', e);
+            });
+          console.log(
+            'Login success with permissions: ' +
+              result.grantedPermissions.toString(),
+          );
+        }
+      },
+      function (error) {
+        console.log('Login fail with error: ' + error);
+      },
+    );
+  };
   return (
     <View>
       <TouchableOpacity style={BUT_TON} onPress={signIn}>
         <Text>35213156</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={BUT_TON} onPress={signIn_Fb}>
+        <Text>login fb</Text>
+        <View>
+          {/* <LoginButton
+            onLoginFinished={(error, result) => {
+              if (error) {
+                console.log('login has error: ' + error);
+              } else if (result.isCancelled) {
+                console.log('login is cancelled.');
+              } else {
+                console.log('Vai lz chim en', result);
+
+                AccessToken.getCurrentAccessToken()
+                  .then((data: any) => {
+                    console.log(data.accessToken.toString());
+                  })
+                  .catch(e => {
+                    console.log('E', e);
+                  });
+              }
+            }}
+            onLogoutFinished={() => console.log('logout.')}
+          /> */}
+        </View>
       </TouchableOpacity>
     </View>
   );
